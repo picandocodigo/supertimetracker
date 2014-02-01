2a9f2a54f3e98e6a070ef783285f0566a333df31
Fernando Briano <fernando@picandocodigo.net>
2014-02-01 21:00:37 -0200

    Adds CSV and JSON export and lots of styles

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var date = new Date(0,0,0,0);
-
 var time = document.getElementById("time");
 var save = document.getElementById("save");
-
 var running = false;
 var interval;
+var entries = new Array();
 
 time.addEventListener("click", startTimer);
 save.addEventListener("click", saveEntry);
+document.getElementById("get-json").addEventListener("click", getJSON);
+document.getElementById("get-csv").addEventListener("click", getCSV);
 
 function startTimer(){
   if (running){
@@ -17,23 +18,44 @@ function startTimer(){
     running = true;
     interval = window.setInterval(function(){
       date.setSeconds(date.getSeconds() + 1);
-      document.getElementById("seconds").innerHTML = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-      document.getElementById("minutes").innerHTML = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
-      document.getElementById("hours").innerHTML = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+      setTimer();
     }, 1000);
   }
 }
 
 function saveEntry(){
   entry = new Entry(date.getHours() + ":" + date.getMinutes(), document.getElementById("description").value);
+  entries.push(entry);
 
   document.getElementById("results").insertAdjacentHTML("afterend", entry.print());
-  document.getElementById("results-table").style.display = "block";
+  document.getElementById("data").style.display = "block";
 
   date = new Date(0,0,0,0);
   document.getElementById("description").value = "";
-  alert(JSON.stringify(entry));
+  clearInterval(interval);
+  running = false;
+  setTimer();
 }
+
+function getJSON(){
+  if(entries.length > 0){
+    document.getElementById("value").innerHTML = "";
+    for (var i = 0; i < entries.length; i++){
+      document.getElementById("value").innerHTML += JSON.stringify(entries[i]);
+    }
+  }
+}
+
+function getCSV(){
+  if(entries.length > 0){
+    document.getElementById("value").innerHTML = "Date; Description; Time<br/>";
+    for (var i = 0; i < entries.length; i++){
+      document.getElementById("value").innerHTML += getFormattedDate(entries[i].date) + ";" + entries[i].description + ";" + entries[i].time + "<br/>";
+    }
+  }
+}
+
+// Entry object
 
 function Entry(time, description){
   this.date = new Date();
@@ -45,7 +67,7 @@ Entry.prototype.print = function(){
   var result = new Array();
   result = "<tr>";
   result += "<td>";
-  result += this.date.getMonth() + "/" + this.date.getDate() + "/" + this.date.getFullYear();
+  result += getFormattedDate(this.date);
   result += "</td>"
   result += "<td>";
   result += this.description
@@ -55,4 +77,19 @@ Entry.prototype.print = function(){
   result += "</td>"
   result += "</tr>"
   return result;
+}
+
+// Auxiliar
+function formatNumber(number){
+  return (number < 10) ? '0' + number : number;
+}
+
+function setTimer(){
+  document.getElementById("seconds").innerHTML = formatNumber(date.getSeconds());
+  document.getElementById("minutes").innerHTML = formatNumber(date.getMinutes());
+  document.getElementById("hours").innerHTML = formatNumber(date.getHours());
+}
+
+function getFormattedDate(date){
+  return date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
 }
