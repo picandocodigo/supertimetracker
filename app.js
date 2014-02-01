32afe96790ef79c56fd1b6eb0f4053352ff89427
Fernando Briano <fernando@picandocodigo.net>
2014-02-01 18:15:09 -0200

    Better styles, some refactor (OOP JS FTW)

--- app.js
+++ app.js
@@ -16,20 +16,33 @@ function startTimer(){
 }
 
 function saveEntry(){
-  var result = new Array();
-  result = "<tr>";
-  result += "<td>";
-  result += document.getElementById("description").value;
-  result += "</td>"
-  result += "<td>";
-  result += date.getHours() + ":" + date.getMinutes();
-  result += "</td>"
-  result += "</tr>"
+  entry = new Entry(date.getHours() + ":" + date.getMinutes(), document.getElementById("description").value);
 
-  document.getElementById("results").innerHTML = result;
+  document.getElementById("results").insertAdjacentHTML("afterend", entry.print());
   document.getElementById("results-table").style.display = "block";
 
   date = new Date(0,0,0,0);
   document.getElementById("description").value = "";
+}
 
+function Entry(time, description){
+  this.date = new Date();
+  this.time = time;
+  this.description = description;
+}
+
+Entry.prototype.print = function(){
+  var result = new Array();
+  result = "<tr>";
+  result += "<td>";
+  result += this.date.getMonth() + "/" + this.date.getDate() + "/" + this.date.getFullYear();
+  result += "</td>"
+  result += "<td>";
+  result += this.description
+  result += "</td>"
+  result += "<td>";
+  result += this.time;
+  result += "</td>"
+  result += "</tr>"
+  return result;
 }
